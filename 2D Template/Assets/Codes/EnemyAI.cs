4a26be1dd8f13c5f5ff6d910b8f1884c5777fe48
MUKILTEO\1605074 <1605074@mukilteo.wednet.edu>
2025-03-25 13:25:55 -0700

    Working on Enemy AI

--- 2D Template/Assets/Codes/EnemyAI.cs
+++ 2D Template/Assets/Codes/EnemyAI.cs
@@ -5,19 +5,82 @@ using UnityEngine.Diagnostics;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        Roaming,
+        ChaseTarget,
+    }
+
+    private EnemyPathfindingMovement pathfindingMovement;
+    private Vector3 startingPosition;
+    private Vector3 roamPosition;
+
+    private void Awake()
+    {
+        pathfindingMovement= GetComponent<EnemyPathfindingMovement>();
+    }
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        roamPosition = GetRoamingPosition();
+    }
+
     public static Vector3 GetRandomDir()
     {
         return new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
     }
 
-    private Vector3 startingPosition;
+
     private void Start()
     {
         startingPosition = transform.position;
     }
 
+    private void Update()
+    {
+        switch (state)
+        {
+            default:
+            case State.Roaming:
+                pathfindingMovement.MoveTo(roamPosition);
+
+                float reachedPositionDistance = 10f;
+                if (Vector3.Distance(transform.position, roamPosition) < reachedPositionDistance)
+                {
+                    roamPosition = GetRoamingPosition();
+                }
+
+                FindTarget();
+                break;
+            case State.ChaseTarget:
+                pathfindingMovement.MoveToTimer(Player.Instance.GetPosition());
+
+                float attackRange = 10f;
+                if(Vector3.Distance(transform.position, Player.Instance.GetPosition()) < attackRange)
+                {
+
+                }
+                break;
+            }    
+        }
+        
+
+       
+
+    }
+
     private Vector3 GetRoamingPosition()
     {
-        return startingPosition + UtilsClass.GetRandomDir() * Random.Range(10f, 70f);
+        startingPosition + UtilsClass.GetRandomDir() * Random.Range(10f, 70f);
+    }
+
+    private void FindTarget() 
+    {
+        float targetRange = 50f;
+        if (Vector3.Distance(transform.position, Player.Instance.GetPosition()) < targetRange)
+        {
+            state = State.ChaseTarget;
+        }
     }
 }
