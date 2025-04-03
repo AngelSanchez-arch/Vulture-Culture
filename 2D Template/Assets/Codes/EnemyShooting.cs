dc24a9d6c362016aaf729fe371a8d6c15b4e4728
MUKILTEO\1605074 <1605074@mukilteo.wednet.edu>
2025-04-03 13:26:42 -0700

    Projectile Shooting

---
+++ 2D Template/Assets/Codes/EnemyShooting.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if(distance < 4)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
