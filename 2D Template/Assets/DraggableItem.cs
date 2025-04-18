7100f8da69f71cb585cdacc7f1379369f792f605
MUKILTEO\1720986 <Kirby336242@gmail.com>
2025-04-18 12:47:13 -0700

    Build attempt in a bit

--- 2D Template/Assets/DraggableItem.cs
+++ 2D Template/Assets/DraggableItem.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
+using UnityEditor;
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
