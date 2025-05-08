119fce50d98543f6189dff2a1523aa13829ac3c3
MUKILTEO\1721807 <E:\VGD\1721807>
2025-05-08 12:30:27 -0700

    Finally I'm finished with the stun script!!!!

--- 2D Template/Assets/Coctatoo.cs
+++ 2D Template/Assets/Coctatoo.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
-
 public class Coctatoo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Animator animator;
+    private bool canStun;
+	private GameObject nextSpot;
+
+	public Masks mask;
+	public bool coctatoo;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
     }
@@ -13,4 +18,29 @@ public class Coctatoo : MonoBehaviour
     {
         
     }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Stun Spot" && collision.GetComponent<StunSpot>().nextSpot != null)
+		{
+			Debug.Log("dig site");
+			canStun = true;
+			nextSpot = collision.GetComponent<StunSpot>().nextSpot;
+
+
+		}
+		Debug.Log(" Nope");
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == "Stun Spot")
+		{
+			Debug.Log("left Stun site");
+			canStun = false;
+			nextSpot = null;
+
+		}
+		Debug.Log(" Nope");
+	}
 }
---
+++ 2D Template/Assets/StunSpot.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class StunSpot : MonoBehaviour
+{
+	public GameObject nextSpot;
+}
